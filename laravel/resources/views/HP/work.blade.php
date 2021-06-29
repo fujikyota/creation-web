14646578afbd0db47391f73fc341be999d80e0d8
kyo_fuji <hydeout758@gmail.com>
2021-06-30 02:11:03 +0900

    クソシンプルwork

--- laravel/resources/views/HP/work.blade.php
+++ laravel/resources/views/HP/work.blade.php
@@ -1,9 +1,114 @@
 @extends('layout.common')
-@include('layout.header')
-<section>
-    <h1>ミッション</h1>
-    <h1>事業内容</h1>
-    <h1>福利厚生</h1>
-    <h1>数字で見るクリエイション</h1>
+  <header>
+  @include('layout.header')
+  </header>
+<body>
+<div class="l-page">
+<section class="index__work">
+            <div class="work__ttl"></div>
+                <h2 class="work__ttll">私たちの使命</h2>
+            <div class="space-100"></div>
 </section>
+  <div class="l-page__inner">
+    <div class="backgr">
+      <div class="c-lower-eyecatch">
+        <div class="c-lower-eyecatch__image is-animate is-animate-fix">
+        </div>
+        <h1 class="c-lower-eyecatch__title">
+          <div class="c-lower-eyecatch__title-wrap">
+            <div class="c-lower-eyecatch__title-en is-animate is-animate-fix"><span class="js-typo-wrap"><span class="js-typo js-typo--1">A</span><span class="js-typo js-typo--2">b</span><span class="js-typo js-typo--3">o</span><span class="js-typo js-typo--4">u</span><span class="js-typo js-typo--5">t</span></div>
+            <div class="c-lower-eyecatch__title-ja is-animate is-animate-fix">Creationについて</div>
+          </div>
+          <div class="c-lower-eyecatch__title-bg is-animate is-animate-fix"></div>
+        </h1>
+      </div>
+      <div class="c-section-expand">
+        <div class="c-section-expand__body is-animate is-animate-fix">
+          <h2 class="c-section-expand__title">事業内容<br class="u-hidden-pc-l u-hidden-pc-s u-hidden-tablet"><br>
+          <p class="c-section-expand__text">わたしたちの事業はっっっっっっっっっっっっっっっっっっっっっっっっっっっっっs。</p>
+          <p class="c-section-expand__text">dddddddddddd</p>
+          <p class="c-section-expand__text">ccccc</p>
+          <p class="c-section-expand__text">vvvvvvvvv</p>
+          <p class="c-section-expand__text">ddddddddd</p>
+          <p class="c-section-expand__text">cccccc</p>
+          <p class="c-section-expand__text">
+          </p>
+          <p></p>
+        </div>
+        <div class="c-section-expand__bg is-animate is-animate-fix"></div>
+      </div>
+      <div class="c-section-expand js-even">
+        <div class="c-section-expand__body is-animate is-animate-fix">
+          <div class="c-contents-multi-column">
+            <div class="c-contents-multi-column__item">
+              <div class="c-contents-multi-column__item-body">
+                <h3 class="c-contents-multi-column__item-head u-tt-u">Full-Service</h3>
+                <p class="c-contents-multi-column__item-text">aaaaaaaaaaaaaaaaaaaaa</p>
+              </div>
+            </div>
+            <div class="c-contents-multi-column__item">
+              <div class="c-contents-multi-column__item-body">
+                <h3 class="c-contents-multi-column__item-head u-tt-u">Best Quality</h3>
+                <p class="c-contents-multi-column__item-text">Aaaaaaaaaaaaaa</p>
+              </div>
+            </div>
+            <div class="c-contents-multi-column__item">
+              <div class="c-contents-multi-column__item-body">
+                <h3 class="c-contents-multi-column__item-head u-tt-u">Holacracy</h3>
+                <p class="c-contents-multi-column__item-text">aaaaaaaaa</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="c-section-expand__bg is-animate is-animate-fix"></div>
+      </div>
+      <div class="c-section-expand" id="amt-data">
+        <div class="c-section-expand__body is-animate is-animate-fix">
+          <h2 class="c-section-expand__title">DATAで見るCreation<br class="u-hidden-pc-l u-hidden-pc-s u-hidden-tablet"></h2>
+          <p class="c-section-expand__text">各職種や年齢などをご紹介します。<br>
+            Creationでは多様な人材が相互に尊重しあい、協力し、依頼者それぞれのニーズに合ったベスト・クオリティーのサービスを提供しています。</p>
+          <div class="c-contents-with-image-s">
+            <div class="c-contents-with-image-s__item">
+              <div class="c-contents-with-image-s__item-image"><img src="../img/work.png" alt=""></div>
+              <div class="c-contents-with-image-s__item-body">
+                <h3 class="c-contents-with-image-s__item-head c-contents-with-image-s__item-head--key">組織構成(各職種の人数) </h3>
+                <h4 class="c-contents-with-image-s__item-sub-head">【スタッフ：合計595】</h4>
+                <p class="c-contents-with-image-s__item-text">programer 250、<br>
+                  desiner、<br>
+                  バックオフィス、その他 140 <br>
+                  ※海外オフィスを含む</p>
+              </div>
+            </div>
+            <div class="c-contents-with-image-s__item">
+            <div class="c-contents-with-image-s__item-image"><img src="../img/work.png" alt=""></div>
+              <div class="c-contents-with-image-s__item-body">
+                <h3 class="c-contents-with-image-s__item-head c-contents-with-image-s__item-head--key">平均年齢</h3>
+                <h4 class="c-contents-with-image-s__item-sub-head">【スタッフの平均年齢】</h4>
+                <p class="c-contents-with-image-s__item-text">35歳1ヶ月(平均勤続年数：8年0ヶ月)</p>
+              </div>
+            </div>
+            <div class="c-contents-with-image-s__item">
+              <div class="c-contents-with-image-s__item-image"><img src="../img/work.png" alt=""></div>
+              <div class="c-contents-with-image-s__item-body">
+                <h3 class="c-contents-with-image-s__item-head c-contents-with-image-s__item-head--key">年齢の分布</h3>
+                <h4 class="c-contents-with-image-s__item-sub-head">【スタッフ】</h4>
+                <p class="c-contents-with-image-s__item-text">10代 (0.00%)、20代 (39.82%)、30代 (32.55%)、40代 (17.45%)、50代 (8.73%)、60代 (1.45%)、65歳以上 (0.00%)</p>
+              </div>
+            </div>
+            <div class="c-contents-with-image-s__item">
+              <div class="c-contents-with-image-s__item-image"></div>
+              <div class="c-contents-with-image-s__item-body">
+                <h3 class="c-contents-with-image-s__item-head c-contents-with-image-s__item-head--key"> 平均年収</h3>
+                <p class="c-contents-with-image-s__item-text">20〜25：350<br>
+                  25〜30：400<br>
+                  30〜40：800<br>
+                  </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="c-section-expand__bg is-animate is-animate-fix"></div>
+      </div>
+</div>
+</body>
 @include('layout.footer')
